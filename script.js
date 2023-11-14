887a8dbbd25488da3ea4f4320dabfc8c8e6ad7a4
Giordano <zagattigiordano@hotmail.it>
2023-11-14 17:32:37 +0100

    edit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const max = 150000;
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "f630d064ffmsh84bb2040185bd5ap14cc66jsn4c8ffa25bc80",
+    "X-RapidAPI-Key": "58a5ce792fmsh0b5d56955f36af1p12cc71jsne92575a89460",
     "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
   }
 };
@@ -34,8 +34,8 @@ const renderMainAlbum = async () => {
   );
 
   albumLargeImg.src = album.cover_medium;
-//   albumLargeTitle.textContent = album.title;
-  albumLargeTitle.innerHTML = `<a style="text-decoration:none;" href="./album.html?id=${album.id}">${album.title}</a>`
+  //   albumLargeTitle.textContent = album.title;
+  albumLargeTitle.innerHTML = `<a style="text-decoration:none; color: white;" href="./album.html?id=${album.id}">${album.title}</a>`;
   albumLargeContributors.textContent =
     album.contributors.length > 1
       ? `${album.contributors[0].name}, ${album.contributors[1].name}`
@@ -52,11 +52,17 @@ const renderMainAlbum = async () => {
 
 const renderSmallAlbums = async () => {
   const album = await getRandomAlbum();
-  const albumHtml = `<div class="col-md-6 col-lg-4">
+  const albumHtml = `<div class="col-6 col-sm-6 col-lg-4">
       <div class="album-small-card card d-flex flex-row align-items-center">
-        <img class="card-img" src="${album.cover_medium}" alt="" style="width: 100px" />
+        <img class="card-img" src="${
+          album.cover_medium
+        }" alt="" style="width: 100px" />
         <div class="card-body">
-          <p class="card-text">${album.title.length > 35 ? album.title.substring(0,32) + "..." : album.title}</p>
+          <p class="card-text">${
+            album.title.length > 35
+              ? album.title.substring(0, 32) + "..."
+              : album.title
+          }</p>
         </div>
       </div>
     </div>`;
@@ -70,15 +76,22 @@ const renderSmallAlbums = async () => {
 const renderMediumAlbums = async () => {
   const album = await getRandomAlbum();
   const albumHtml = `<div class="col-md-6 col-lg-3">
-    <div class="album-medium-card card" >
-      <img
+    <div class="album-medium-card card d-flex flex-column" >
+    <div class="medium-temp-div ">  
+    <img
         class="card-img-top"
-        src="${album.cover_medium}"
+        src="${album.cover_medium}" 
         alt="Card image cap"
       />
       <div class="card-body">
-        <p class="card-text">${album.title.length > 26 ? album.title.substring(0,23) + "..." : album.title}</p>
+        <p class="card-text">${
+          album.title.length > 26
+            ? album.title.substring(0, 23) + "..."
+            : album.title
+        }</p>
       </div>
+      </div>
+      
     </div>
   </div>`;
 
@@ -88,17 +101,34 @@ const renderMediumAlbums = async () => {
   return tempDiv.firstElementChild;
 };
 
+const renderTitles = async () => {
+  const album = await getRandomAlbum();
+  const playlistHTML = `<p>${album.title}</p>`;
+
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = playlistHTML;
+
+  return tempDiv.firstElementChild;
+  
+};
+
 const renderAlbums = () => {
   renderMainAlbum();
 
+  for (let z = 0; z < 40; z++){
+    renderTitles().then((data) => document.getElementById("playlist").appendChild(data))
+  }
+
   for (let j = 0; j < 6; j++) {
-    renderSmallAlbums().then(data => document.getElementById("row-1").appendChild(data))
-    // document.getElementById("row-1").appendChild(renderSmallAlbums());
+    renderSmallAlbums().then((data) =>
+      document.getElementById("row-1").appendChild(data)
+    );
   }
 
   for (let i = 0; i < 8; i++) {
-    // document.getElementById("row-2").appendChild(renderMediumAlbums());
-    renderMediumAlbums().then(data => document.getElementById("row-2").appendChild(data))
+    renderMediumAlbums().then((data) =>
+      document.getElementById("row-2").appendChild(data)
+    );
   }
 };
 
